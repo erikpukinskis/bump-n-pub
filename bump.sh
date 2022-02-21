52137c7288db93311fa27db36f442d54abc4413b
Erik Pukinskis <erik@snowedin.net>
2022-02-20 18:33:41 -0700

    Make conditionals POSIX compliant (hopefully)

--- bump.sh
+++ bump.sh
@@ -1,6 +1,8 @@
 #/bin/bash
 arg=$1
 
+ps -p $$
+
 usage () {
   echo ""
   echo "Usage:"
@@ -8,27 +10,24 @@ usage () {
   echo ""
 }
 
-if ! [[ $arg ]]; then
+if [ $# -eq 0 ]; then
   echo "Error: Must provide a level"
   usage
   exit 1
 fi
 
-levels='major|minor|patch|premajor|preminor|prepatch|prerelease'
-
-if ! [[ $arg =~ $levels ]]; then
+if echo $arg | grep -Eqv '^(major|minor|patch|premajor|preminor|prepatch|prerelease)$'; then
   echo "Error: Invalid version: $1"
   usage
   exit 1
 fi
 
-if [[ $(git diff --stat) != '' ]]; then
+
+if git diff --stat | grep -E '.'; then
   echo "Error: Working directory must be clean to bump'n'pub"
   git diff --stat
   exit 1
 fi
-
-
 
 git commit -m "v`npm version $arg`"
 npx json -f package.json -I -e "delete this.devDependencies"
