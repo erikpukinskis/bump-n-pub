8727fcde166273bf2413558e0ccdf02cbc6165d0
Erik Pukinskis <erik@snowedin.net>
2022-05-08 20:04:27 -0600

    add back a getopts

--- bump.sh
+++ bump.sh
@@ -3,6 +3,32 @@ newversion=$1
 flag=$2
 arg_count=$#
 
+usage() { echo "Usage: $0 [-s <45|90>] [-p <string>]" 1>&2; exit 1; }
+
+while getopts ":s:p:" o; do
+    case "${o}" in
+        s)
+            s=${OPTARG}
+            ((s == 45 || s == 90)) || usage
+            ;;
+        p)
+            p=${OPTARG}
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ -z "${s}" ] || [ -z "${p}" ]; then
+    usage
+fi
+
+echo "s = ${s}"
+echo "p = ${p}"
+
+exit
 ##################
 # Helper functions
 
